a018a96329d4602ccedc2595f0f17d48b60da81f
bigmistqke <vandijckv@gmail.com>
2023-01-09 22:40:27 +0100

    update types, add jsdoc

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,48 @@
-import { ComponentProps, createEffect, createSignal, on } from "solid-js";
+import { ComponentProps, createEffect, createSignal, on, onMount } from "solid-js";
 import calculateNodeHeight from "./calculateNodeHeight";
 import getSizingData, { SizingData } from "./getSizingData";
 import { useWindowResizeListener } from "./hooks";
 
 type TextareaProps = ComponentProps<"textarea">;
 
-type Style = Omit<NonNullable<TextareaProps["style"]>, "max-height" | "min-height"> & {
-  height?: number;
+type Style = NonNullable<TextareaProps["style"]> & {
+  height?: never;
+  "max-height"?: never;
+  "min-height"?: never;
 };
 
 export type TextareaHeightChangeMeta = {
   rowHeight: number;
 };
 export interface TextareaAutosizeProps extends Omit<TextareaProps, "style"> {
+  /**
+   * Maximum number of rows up to which the textarea can grow
+   */
   maxRows?: number;
+  /**
+   * Minimum number of rows to show for textarea
+   */
   minRows?: number;
+  /**
+   * Setting rows is not allowed.
+   * Use maxRows and minRows instead.
+   */
+  rows?: never;
+  /**
+   * Function invoked on textarea height change, with height as first argument.
+   * The second function argument is an object containing additional information
+   * that might be useful for custom behaviors.
+   * Current options include { rowHeight: number }.
+   */
   onHeightChange?: (height: number, meta: TextareaHeightChangeMeta) => void;
+  /**
+   * Reuse previously computed measurements when computing height of textarea. Default: false
+   */
   cacheMeasurements?: boolean;
   style?: Style;
 }
 
-function TextareaAutosize(
-  props: {
-    cacheMeasurements?: boolean;
-    maxRows?: number;
-    minRows?: number;
-    oninput?: (event: InputEvent) => void;
-    ref?: (textarea: HTMLTextAreaElement) => void;
-    onHeightChange?: (height: number, { rowHeight }: { rowHeight: number }) => void;
-  } & TextareaProps,
-) {
+function TextareaAutosize(props: TextareaAutosizeProps & TextareaProps) {
   const [textarea, setTextarea] = createSignal<HTMLTextAreaElement>();
   let heightRef = 0;
   let measurementsCacheRef: SizingData | undefined = undefined;
